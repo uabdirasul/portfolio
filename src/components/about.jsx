9701df575aff17bae1c1ca9f832be215412346b2
uabdirasulm <ubbiniyazov04@gmail.com>
2023-06-25 20:06:58 +0500

    change section info text class

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 
-// Style
-import "../scss/components/_about.scss";
-
 const About = () => {
   return (
     <div className="about">
       <div className="about__container">
-        <h2 className="about__header section-header">About</h2>
-        <p className="about__info">
+        <h2 className="section-header">About</h2>
+        <p className="section-info-text">
           Assalam Alaikum. I enjoy building dynamic and creative products from
           start to finish. My focus is on developing intuitive experiences that
           constantly grow and improve based on user metrics. I’m always looking
