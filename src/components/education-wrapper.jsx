00b0b64074508cb3d6b304ecb2f2d89591983d59
uabdirasulm <ubbiniyazov04@gmail.com>
2023-08-06 12:31:56 +0500

    Add js course

--- src/components/education-wrapper.jsx
+++ src/components/education-wrapper.jsx
@@ -8,16 +8,22 @@ const EducationWrapper = () => {
     <div className="education">
       <h2 className="section-header">Education</h2>
       <Education
-        courseYear="2021 July"
+        courseYear="2020 Oct"
+        courseName="Interactivity with JavaScript"
+        courseLink="https://www.coursera.org/learn/javascript/"
+        courseTechnologies="JavaScript • JQuery"
+      />
+      <Education
+        courseYear="2021 Jul"
         courseName="0 to Hero"
         courseLink="https://old.mohirdev.uz/courses/javascript-toliq-kurs-from-0-to-hero/"
-        courseTechnologies="JavaScript • OOP • Git • GitHub • npm • Babel • Webpack"
+        courseTechnologies="JavaScript • OOP • Git • GitHub • npm"
       />
       <Education
         courseYear="2023 May"
         courseName="Interactive React va Redux"
         courseLink="https://telegra.ph/Interaktiv-React-va-Redux-07-31"
-        courseTechnologies="React • Redux • Redux Thunk • Version Control • Styled Component • Axios"
+        courseTechnologies="React • Redux • Redux Thunk • Version Control • Axios"
       />
     </div>
   );
